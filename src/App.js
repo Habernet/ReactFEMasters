c9b4156da77665b30fd0ffd3491285f47e32dfe6
habernet <phaberern@gmail.com>
2019-10-08 15:56:00 -0400

    Add parcel and script, Pet.js in separate file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 //Create your App "stamp"
 const App = () => {
@@ -21,4 +17,4 @@ const App = () => {
 };
 
 // Use your "stamp" and render it to the page
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
