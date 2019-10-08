27c32c7d027e3e2343f2c10bf8a84bf38db14231
habernet <phaberern@gmail.com>
2019-10-08 16:58:49 -0400

    Converted to JSX

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import Pet from "./Pet";
 
 //Create your App "stamp"
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Zoey",
-      animal: "dog",
-      breed: "Golden Doodle"
-    }),
-    React.createElement(Pet, { name: "Maya", animal: "dog", breed: "Mutt" }),
-    React.createElement(Pet, { name: "Evee", animal: "cat", breed: "Tabby" })
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Zoey",
+  //     animal: "dog",
+  //     breed: "Golden Doodle"
+  //   }),
+  //   React.createElement(Pet, { name: "Maya", animal: "dog", breed: "Mutt" }),
+  //   React.createElement(Pet, { name: "Evee", animal: "cat", breed: "Tabby" })
+  // ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Zoey" animal="dog" breed="Golden doodle" />
+      <Pet name="Maya" animal="dog" breed="Mutt" />
+      <Pet name="Evee" animal="cat" breed="Tabby" />
+    </div>
+  );
 };
 
 // Use your "stamp" and render it to the page
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
